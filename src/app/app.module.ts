39412347169a8936fd5a2e6eb6a4feac303396dc
João <joaosaraivasapori@gmail.com>
2024-04-24 11:05:19 -0300

    car.component.html estruturado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { FromComponent } from './from/from.component';
 import { FormComponent } from './form/form.component';
 import { CardComponent } from './card/card.component';
 import { ButtonComponent } from './button/button.component';
@@ -10,7 +9,6 @@ import { ButtonComponent } from './button/button.component';
 @NgModule({
   declarations: [
     AppComponent,
-    FromComponent,
     FormComponent,
     CardComponent,
     ButtonComponent
