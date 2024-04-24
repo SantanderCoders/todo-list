fae58b05f9f86398e194bf481cd4ff49d431ffb6
João <joaosaraivasapori@gmail.com>
2024-04-24 10:46:44 -0300

    Criação dos componentes card, button e form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FromComponent } from './from/from.component';
+import { FormComponent } from './form/form.component';
+import { CardComponent } from './card/card.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FromComponent,
+    FormComponent,
+    CardComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule
